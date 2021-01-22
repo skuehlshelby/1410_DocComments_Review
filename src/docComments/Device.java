494ced0d7c0ded4f86f367524a894ecd0a4af30a
Zader000 <77822070+Zader000@users.noreply.github.com>
2021-01-21 22:27:48 -0700

    Update Device.java
    I added some ideas, thoughts, and doc comments to the file.

--- src/docComments/Device.java
+++ src/docComments/Device.java
@@ -4,21 +4,26 @@ public class Device
 {
     //Shared members
     private static int id = 1111111;
-
-
-
+    //I was thinking that we could just do Xbox or PlayStation
+    String brand, model, storageSpace;
+    
+    //Cool idea to do the count and create the id at the same time. I think there would be an issue where if the user wanted to get the id that it would mess it up.
+    //So we would need to have a different method for getting the id and another for creating it and assigning it. 
     private static int getID()
     {
         return id++;
     }
 
     //Instance members
-    public Device(String thingOne, String thingTwo, String thingThree)
+    /**
+    * Constructor for a device object
+    */
+    public Device(String brand, String model, String storageSpace)
     {
         ID = getID();
-        this.thingOne = thingOne;
-        this.thingTwo = thingTwo;
-        this.thingThree = thingThree;
+        this.brand = brand;
+        this.model = model;
+        this.storageSpace = storageSpace;
     }
 
     private final int ID;
@@ -26,25 +31,33 @@ public class Device
     private final String thingTwo;
     private final String thingThree;
 
-
-    public String getThingOne()
+    /**
+    * Returns the brand of the device
+    */
+    public String getBrand()
     {
-        return thingOne;
+        return brand;
     }
-
-    public String getThingTwo()
+    /**
+    * Returns the model of the device
+    */
+    public String getModel()
     {
-        return thingTwo;
+        return model;
     }
-
-    public String getThingThree()
+    /**
+    * Returns the HDD storage space on the device.
+    */
+    public String getStorageSpace()
     {
-        return thingThree;
+        return storageSpace;
     }
-
+    /**
+    * Gathers all information about the device and converts it into a string. 
+    */
     @Override
     public String toString()
     {
-        return String.format("ID: %d, Thing One: %s, Thing Two: %s, Thing Three: %s", ID, getThingOne(), getThingTwo(), getThingThree());
+        return String.format("ID: %d, Brand: %s, Model: %s, Storage Space: %s", ID, getBrand(), getModel(), getStorageSpace());
     }
 }
