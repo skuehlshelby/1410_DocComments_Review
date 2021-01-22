a4d3074abdc1c9f01ea4154ca7082d073b6bea17
Scott K <georgelarry99@gmail.com>
2021-01-21 20:02:32 -0700

    Initial Commit.

---
+++ src/docComments/Device.java
@@ -0,0 +1,50 @@
+package docComments;
+
+public class Device
+{
+    //Shared members
+    private static int id = 1111111;
+
+
+
+    private static int getID()
+    {
+        return id++;
+    }
+
+    //Instance members
+    public Device(String thingOne, String thingTwo, String thingThree)
+    {
+        ID = getID();
+        this.thingOne = thingOne;
+        this.thingTwo = thingTwo;
+        this.thingThree = thingThree;
+    }
+
+    private final int ID;
+    private final String thingOne;
+    private final String thingTwo;
+    private final String thingThree;
+
+
+    public String getThingOne()
+    {
+        return thingOne;
+    }
+
+    public String getThingTwo()
+    {
+        return thingTwo;
+    }
+
+    public String getThingThree()
+    {
+        return thingThree;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("ID: %d, Thing One: %s, Thing Two: %s, Thing Three: %s", ID, getThingOne(), getThingTwo(), getThingThree());
+    }
+}
---
+++ src/docComments/Main.java
@@ -0,0 +1,9 @@
+package docComments;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+	// write your code here
+    }
+}
